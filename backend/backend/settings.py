6da126821448cec332781324387c98ebf479cda3
ladamsperez <ladamsperezwork@gmail.com>
2021-10-14 09:09:56 -0400

    added app, drf,  and app config to intalled apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # CORS
+    'corsheaders',
+
+    # Django REST framework 
+    'rest_framework',
+    
+    # 1337 application 
+    'e1337.apps.E1337Config',
+
+    #app
+    'e1337'
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
