47cb545d6e6954761b335e9b0348bf0d5cbe267f
ladamsperez <ladamsperezwork@gmail.com>
2021-10-14 09:40:34 -0400

    managing serialization & deserialization from JSON

---
+++ backend/e1337/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers 
+from e1337.models import E1337
+ 
+ 
+class E1337Serializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = E1337
+        fields = ('id',
+                  'title',
+                  'description',
+                  'published')
