9dc84bf7a096b8cbffb2a5f0b5c23d30fce70976
ladamsperez <ladamsperezwork@gmail.com>
2021-10-14 09:48:38 -0400

    created dummy urls for testing api local

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,21 +1,10 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from e1337 import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/e1337', views.e1337_list),
+    path('api/e1337/<int:pk>/', views.e1337_detail),
+    path('api/e1337/published', views.e1337_list_published)
 ]
